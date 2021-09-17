6dc02ee2bfd123f7cdbbc046915ae7bfc86532a1
maicol <maicol.matteoli@gmail.com>
2021-09-17 14:23:24 +0200

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header';
+import Main from '../components/Main.vue';
+export default {
+  name: "App",
+  components:{
+    Header,
+    Main
+  }
+}
+</script> 
+
+<style lang="scss" scoped>
+
+</style>
